00bb3224d8cfd147f03c978670673568b7f4f5c0
R.D. Wagner <callisto076@gmail.com>
2022-04-07 13:20:24 -0500

    Update PID node to include ball following subscription

--- src/ballFollower.cpp
+++ src/ballFollower.cpp
@@ -1,11 +1,38 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "std_msgs/Int32.h"
+#include "ballDetector/ballLocation.h" 
 
 #include <sstream>
 
-void ballLocationCallback(const std_msgs::Int64 data)
+// Initialize variables 
+std_msgs::Int32 targetAngle;
+std_msgs::Int32 targetDistance;
+
+// 
+void ballLocationCallback(const ballDetector::ballLocation ballLocation)
 {
-    // do some math
+    ROS_INFO("Image Width: %d | Image Height: %d | X Pos: %f | Y Pos: %f | Radius: %f", ballLocation.imageWidth, ballLocation.imageHeight, ballLocation.x, ballLocation.y, ballLocation.radius);
+
+    if (ballLocation.x > 0) {
+
+        targetAngle = -ballLocation.x; 
+    }
+    
+    else if (ballLocation.x < 0) {
+
+        targetAngle = abs(ballLocation.x);
+    }
+
+    else if (ballLocation.radius > 500) {
+
+        targetDistance = -100;
+    }
+    
+    else if (ballLocation.radius < 500 {
+
+        targetDistance = 100;
+    }
+
 }
 
 int main(int argc, char **argv)
@@ -17,14 +44,16 @@ int main(int argc, char **argv)
 
     ros::Subscriber sub = n.subscribe("ballLocation", 1000, ballLocationCallback);
     
-    ros::Publisher chatter_pub = n.advertise<std_msgs::Int64>("BalboaLL", 1000);
-
+    ros::Publisher pubRot = n.advertise<std_msgs::Int32>("rotation", 1000);
+    ros::Publisher pubDist = n.advertise<std_msgs::Int32>("distance", 1000);
+    
     ros::Rate loop_rate(10);
 
     int count = 0;
     while (ros::ok())
     {
-        chatter_pub.publish(/*something*/);
+        pubRot.publish(targetAngle);
+        pubDist.publish(targetDistance);
 
         ros::spinOnce();
         loop_rate.sleep();
--- src/pidLab3.py
+++ src/pidLab3.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from geometry_msgs.msg import Twist
+from ball_detector.msg import ballLocation
 from balboa_core.msg import balboaMotorSpeeds
 from balboa_core.msg import balboaLL
 import rospy
 
+# Must start camera & detect ball to run PID 
+
+# Declare variables 
 pubMotor = rospy.Publisher('/motorSpeeds', balboaMotorSpeeds, queue_size = 10)
 motor = balboaMotorSpeeds()
 
 targetAngle = 0
 targetDistance = 0
 
+# PID constants
 pValAng = 0.0001
 dValAng = 0.00000001
 pValDis = 0.015
 dValDis = 0.0001
 
+# We are using hard-coded constants, not from a launch file
 # pValAng = rospy.get_param('/pidLab1/pValAng')
 # dValAng = rospy.get_param('/pidLab1/dValAng')
 # pValDis = rospy.get_param('/pidLab1/pValDis')
 # dValDis = rospy.get_param('/pidLab1/dValDis')
 
-
-initFlag = 0
+# initFlag = 0
 initIMU = balboaLL()
 
-
-
+# PID function based on robot data (/balboaLL)
 def callbackIMU(data):
     global initFlag
     global targetAngle
@@ -34,24 +37,64 @@ def callbackIMU(data):
     global oldAngError
     global oldDisError
     
+    # We do not take our initial first position to feed as the target, we start directly from the camera info
     # if(initFlag == 0):
     #     initIMU = data
     #     initFlag = 1
     #     targetAngle = initIMU.angleX
     #     targetDistance = initIMU.distanceRight
 
+    # PID Error calculations for both angle and distance
     angError = (targetAngle - data.angleX)
     disError = (targetDistance - data.distanceRight)
 
+    # PID value calculations for both angle and distance
     angPidVal = (pValAng * angError) + (dValAng * (angError - oldAngError)) 
     disPidVal = (pValDis * disError) + (dValDis * (disError - oldDisError))
     
+    # Updated commanded motors values based on angle and distance PID values 
     motor.right = angPidVal + disPidVal
     motor.left = -angPidVal + disPidVal
 
+    # Set derivative values 
     oldAngError = angError
     oldDisError = disError
-def 
+
+# Ball follower function    
+def callbackTarget(ballInfo):
+
+    rospy.loginfo("Radius: %d", ballInfo.radius)
+
+    #     if (ballLocation.x > 0) {
+
+    #     targetAngle = -ballLocation.x; 
+    # }
+    
+    # else if (ballLocation.x < 0) {
+
+    #     targetAngle = abs(ballLocation.x);
+    # }
+
+    # else if (ballLocation.radius > 500) {
+
+    #     targetDistance = -100;
+    # }
+    
+    # else if (ballLocation.radius < 500 {
+
+    #     targetDistance = 100;
+    # }
+
+def main(): 
+    rospy.init_node('pidLab3')
+    rospy.loginfo("STARTED")
+    rospy.Subscriber('/ballLocation',ballLocation,callbackTarget)
+    rate = rospy.Rate(10)   
+
+    # Continously publish motor speeds based on PID
+    while not rospy.is_shutdown():
+        pubMotor.publish(motor)
+        rate.sleep()    
 
 
 
