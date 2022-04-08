ed642e0d76456f622a532f8ad90730edd646746c
root <root@439robot1>
2022-04-07 23:01:46 -0500

    lab 3 final

--- src/pidLab3.py
+++ src/pidLab3.py
@@ -10,82 +10,71 @@ import rospy
 # Declare variables 
 pubMotor = rospy.Publisher('/motorSpeeds', balboaMotorSpeeds, queue_size = 10)
 motor = balboaMotorSpeeds()
+#ball = ballLocation()
 
-targetAngle = 0
-targetDistance = 0
+oldAngleError = 0
+oldDistanceError = 0
+angleRunningAvg = 0
+distanceRunningAvg = 0
 
-# PID constants
-pValAng = 0.0001
-dValAng = 0.00000001
-pValDis = 0.015
-dValDis = 0.0001
-
-# We are using hard-coded constants, not from a launch file
-# pValAng = rospy.get_param('/pidLab1/pValAng')
-# dValAng = rospy.get_param('/pidLab1/dValAng')
-# pValDis = rospy.get_param('/pidLab1/pValDis')
-# dValDis = rospy.get_param('/pidLab1/dValDis')
-
-# initFlag = 0
-initIMU = balboaLL()
-
-# PID function based on robot data (/balboaLL)
-def callbackIMU(data):
-    global initFlag
-    global targetAngle
-    global targetDistance
-    global oldAngError
-    global oldDisError
-    
-    # We do not take our initial first position to feed as the target, we start directly from the camera info
-    # if(initFlag == 0):
-    #     initIMU = data
-    #     initFlag = 1
-    #     targetAngle = initIMU.angleX
-    #     targetDistance = initIMU.distanceRight
-
-    # PID Error calculations for both angle and distance
-    angError = (targetAngle - data.angleX)
-    disError = (targetDistance - data.distanceRight)
-
-    # PID value calculations for both angle and distance
-    angPidVal = (pValAng * angError) + (dValAng * (angError - oldAngError)) 
-    disPidVal = (pValDis * disError) + (dValDis * (disError - oldDisError))
-    
-    # Updated commanded motors values based on angle and distance PID values 
-    motor.right = angPidVal + disPidVal
-    motor.left = -angPidVal + disPidVal
-
-    # Set derivative values 
-    oldAngError = angError
-    oldDisError = disError
+#initFlag = 0
 
 # Ball follower function    
-def callbackTarget(ballInfo):
+def callbackTarget(ball):
+    global oldAngleError
+    global oldDistanceError
+    global angleRunningAvg
+    global distanceRunningAvg
 
-    rospy.loginfo("Radius: %d", ballInfo.radius)
+#    global initFlag
 
-    #     if (ballLocation.x > 0) {
+    # Start with zero motor speed initially
+ #   if(initFlag == 0):
+ #       initIMU = ball
+ #       initFlag = 1
+ #       targetAngle = 0
+ #       targetDistance = 140
 
-    #     targetAngle = -ballLocation.x; 
-    # }
+    # PID VALUES
+    pAngle = 0.015
+    dAngle = 0.05
+    pDistance = 0.07
+    dDistance = 0.01
+
+    # target distance
+    targetDistance = 130
     
-    # else if (ballLocation.x < 0) {
-
-    #     targetAngle = abs(ballLocation.x);
-    # }
-
-    # else if (ballLocation.radius > 500) {
-
-    #     targetDistance = -100;
-    # }
+    # Calculate error
+    angleError = ball.x
+    distanceError = ball.radius - targetDistance
     
-    # else if (ballLocation.radius < 500 {
+    # PID EQUATIONS
+    anglePIDValue = (pAngle * angleError) + (dAngle * (angleError - oldAngleError)) 
+    distancePIDValue = (pDistance * distanceError) + (dDistance * (distanceError - oldDistanceError))
 
-    #     targetDistance = 100;
-    # }
+    angleRunningAvg = (angleRunningAvg * 0.9) + (anglePIDValue * 0.1)
+    distanceRunningAvg = (distanceRunningAvg * 0.9) + (distancePIDValue * 0.1)
 
-def main(): 
+
+    # Save old error values for derivative pid equation
+    oldAngleError = angleError
+    oldDistanceError = distanceError
+
+    # Assign motor speeds
+    #motor.left = distancePIDValue + anglePIDValue
+    #motor.right = distancePIDValue - anglePIDValue
+
+    motor.left = distanceRunningAvg + angleRunningAvg
+    motor.right = distanceRunningAvg - angleRunningAvg
+
+    # debug output
+    rospy.loginfo("-------------------------")
+    rospy.loginfo("Radius: %d", ball.radius)
+    rospy.loginfo("x: %d", ball.x)
+    rospy.loginfo("left motor: %f", motor.left)
+    rospy.loginfo("right motor: %f", motor.right)
+
+def mainFunction(): 
     rospy.init_node('pidLab3')
     rospy.loginfo("STARTED")
     rospy.Subscriber('/ballLocation',ballLocation,callbackTarget)
@@ -94,7 +83,13 @@ def main():
     # Continously publish motor speeds based on PID
     while not rospy.is_shutdown():
         pubMotor.publish(motor)
+        motor.left = 0
+        motor.right = 0
         rate.sleep()    
 
-
+if __name__ == '__main__':
+    try:
+        mainFunction()
+    except rospy.ROSInterruptException:
+        pass
 
